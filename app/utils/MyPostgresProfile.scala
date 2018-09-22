38d7226511f91d797dc28e8a58e295ed4564ecee
Evan Chan <evan.chanyiksan@gmail.com>
2018-09-22 15:46:04 -0700

    (#24) Fix unknown type 'status'

--- app/utils/MyPostgresProfile.scala
+++ app/utils/MyPostgresProfile.scala
@@ -7,7 +7,7 @@ trait MyPostgresProfile extends ExPostgresProfile with PgEnumSupport {
   override val api = MyAPI
 
   object MyAPI extends API {
-    implicit val statusTypeMapper = createEnumJdbcType("status", models.Status)
+    implicit val statusTypeMapper = createEnumJdbcType("status_enum", models.Status)
     implicit val labelListTypeMapper =
       createEnumListJdbcType("label_enum", models.Label)
   }
